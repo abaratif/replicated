c98815f159c40dd415009acc68701bdf12633114
Arman Baratifar <ab405h@att.com>
2017-08-15 10:44:02 -0500

    Added POST endpoint and examples to readme

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
     This project requires python2 and uses virtualenv for development.
 
-### Running Locally
+### Installing and Running Locally
 ```
     git clone https://github.com/abaratif/replicated.git
     cd replicated
-    virtualenv -p python3 VENV
+    virtualenv VENV
     source VENV/bin/activate
     pip install -r requirements.txt
     python -m run
@@ -22,4 +22,87 @@ Use nose2 to execute unit tests.
 ```
 	nose2 -v
 ```
+
+### API Spec
+
+This project uses flask-swagger to provide a clear API specification. The API spec lives at the ```/api/v1.0/spec``` endpoint. For easy viewing, install the Swagger UI Console extension for Chrome.
+
+## Usage
+
+Once the API is spun up, POST requests can be sent to the ```/api/v1.0/keys``` endpoint. See the following curl command for an example.
+
+### Example, Two Users with Keys
+
+In this example, a request is made to obtain keys for two users, using the following curl command:
+
+```
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"users" : ["gvanrossum", "kennethreitz"]}' 'localhost:5000/api/v1.0/keys'
+```
+
+Below is the corresponding response.
+```
+{
+  "code": "success", 
+  "data": {
+    "gvanrossum": [
+      {
+        "id": 7189340, 
+        "key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSbJq1XbEC3WmjUNyLchy57svUGespG3WMnf7WpxzLWsfsTrLuxjeogJ/+EznE8k0mgllGjbB9DdRlZf9AXfrUyO27Zse5uVVc4hww+5YANUE0sqDyJYWqrl65/Zm59Eqv4WeLjg16Ory14vUKAm3/KVvvpesmP/F/1gRYzcV3L5TSz7UQzXvdm+arL9UUq0XiIHGdcnkNWOnh2BcXc2uxyijsyUMpWbwtk6TW82AVepxTg2qZ2jWs/5rdHDnS+jbgih+0OQHXHoUTl37XP2QM/KMwkJqBa1KW1BHhyAoRO7Vx7Q4GegDVgkD031qzTXfvsGsXPyFNYK653enI5UTL"
+      }
+    ], 
+    "kennethreitz": [
+      {
+        "id": 20116054, 
+        "key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSkZaNWJpkvIvJSDHpYiBAwyNAEgKGStitUQUf1iBeYecvQb1Jszgfh47EuLYJOz8Ab6avJbHEjafUMWH7E2tSlsYRcWFAdkTwdIG6jvTLhZHmGM3wgWHUqgwEflcTBsqH9Eg+PViOU5CDQrJJxARCwLIKqgAvObzTHjk5oWzJ33LZJ/Y/OJUrKH8I72xZw3wGcY/yblBgKV+/IKfaM9EgafgKuhtGtNHY+o8gkMsLBhbWR5X608vPG9KrYs9lXTuL+rOq1iRb+YEjOXynudpCtILCGZgd4blCNz4JoBq8L/02vhutD0IquVUfR9AoRzgUmC84Lp3mxJT6hDCb+RlV"
+      }, 
+      {
+        "id": 23104123, 
+        "key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDK4xIPRGfHYCxjQUGdFK4ORcljTo6tMrCNsprh/WKnJNNSWquu0ASx2M8OYy05YVHaakT0zmTBpwPc56wK6LHaPFZSF3eUfOXck3WuNGpTlWdiogFax+oJXc5uW9VJL0JaHWtYY98Ay36idwA91ac806WThEdIcYU6Q/ARcc+4SRnQXMR6dEf+tX5EaOfU/o6Gyn6EwCKeb2C2fGXcvc+GNzCSGVxWxnXxm3wn1jOmX8fMpVlyh8aqzObApl6BI+hJO6W3NjIIe67TyHtdZkU1eWXZRpNTqA58uOycNV9nrCXMYMfBo+chbO8kL0ZlD02AdD9+UHSPD8g5f715Hk4f"
+      }, 
+      {
+        "id": 24330972, 
+        "key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjfCIDU1uzRPTc3V/JdTYtmXW/XCJSfyyIs7HmWx8HtPZEYEOzJrHxMExEca96XSLRlUEEscWszlwgvmaUWJb+cBs2hlEXeuNvh/CXOKEUJo0JaGFstO7X7U9UBHBJO/ibTv3LwwnhV1IfcE77uHnHSMihpnLfrd68dzX9lD99G8EtExD0LKhVsRMNp7ap9TYpsxCenna85N632Z6igW5rFTAVsy4LDUW0hcNeNvkIo5NDCYNHAzdDZ3jfvQSChlE3rZGRkbXA8hCHTQwABWs3ZoG4LTy4EaFyx1F5SJsw3VvmT9csSGYKSKY72/uAvhpyuZDPBOdAT4QM0Kq7r6Wh"
+      }, 
+      {
+        "id": 24514479, 
+        "key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCgdNG4uSkbmPL28oYiAi0UN47sPyJ7k8RocmODzg2J0WCetnES1oQ6hLBCjun3lAFUs+DpEfvgqO3zjZrfONZrg9DSxj124uHCXI1dG5/QaftbiPx3OqOz3uA7rG9F0VxzWkXcevqsQPi2nODCsG+kxa8swmlSktSn47s0azR4Hw081j4q2CRP/gZwjNdIyVIRQtO0jrXkIsHL72zmdATgpSx4WdFxaMlWIfAvF8Xp7JuQXrTn3EaEG/0Iwpkyyd7UnpDFCGYWqEfWn1vI4OI9WCD6qywfQCATs7XgiWbmOHI+Ni6mz1RUn8a3QiCw8WoioTwkwpPLMLaWNOR3EnW9"
+      }
+    ]
+  }
+}
+```
+### Example, one user with no keys
+
+If a user does not have any public keys, an empty array will be returned for that user.
+
+```
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"users" : ["abaratif"]}' 'localhost:5000/api/v1.0/keys'
+```
+
+```
+{
+  "code": "success", 
+  "data": {
+    "abaratif": []
+  }
+}
+
+```
+
+### Example, invalid username
+
+If a user does not exist on github, an error will be returned.
+
+```
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"users" : ["abaratif_is_cool"]}' 'localhost:5000/api/v1.0/keys'
+```
+
+```
+{
+  "code": "invalidInput", 
+  "error": "Not Found", 
+  "message": "Invalid input"
+}
+```
+
 
--- api/routes/routes_general.py
+++ api/routes/routes_general.py
@@ -10,171 +10,22 @@ from api.models.model_author import Author, AuthorSchema
 
 route_path_general = Blueprint("route_path_general", __name__)
 
-# @route_path_general.route('/v1.0/authors', methods=['POST'])
-# def create_author():
-#     """
-#     Create author endpoint
-#     ---
-#     parameters:
-#         - in: body
-#           name: body
-#           schema:
-#             id: Author
-#             required:
-#                 - name
-#                 - surname
-#                 - books
-#             properties:
-#                 name:
-#                     type: string
-#                     description: First name of the author
-#                     default: "John"
-#                 surname:
-#                     type: string
-#                     description: Surname of the author
-#                     default: "Doe"
-#                 books:
-#                     type: string
-#                     description: Book list of author
-#                     type: array
-#                     items:
-#                         schema:
-#                             id: BookSchema
-#                             properties:
-#                                 title:
-#                                     type: string
-#                                     default: "My First Book"
-#                                 year:
-#                                     type: date
-#                                     default: "1989-01-01"
-#     responses:
-#             200:
-#                 description: Author successfully created
-#                 schema:
-#                   id: AuthorCreated
-#                   properties:
-#                     code:
-#                       type: string
-#                     message:
-#                       type: string
-#                     value:
-#                       schema:
-#                         id: AuthorFull
-#                         properties:
-#                             name:
-#                                 type: string
-#                             surname:
-#                                 type: string
-#                             books:
-#                                 type: array
-#                                 items:
-#                                     schema:
-#                                         id: BookSchema
-#             422:
-#                 description: Invalid input arguments
-#                 schema:
-#                     id: invalidInput
-#                     properties:
-#                         code:
-#                             type: string
-#                         message:
-#                             type: string
-#     """
-#     try:
-#         data = request.get_json()
-#         author_schema = AuthorSchema()
-#         author, error = author_schema.load(data)
-#         result = author_schema.dump(author.create()).data
-#         return response_with(resp.SUCCESS_200, value={"author": result})
-#     except Exception:
-#         return response_with(resp.INVALID_INPUT_422)
-#
-#
-# @route_path_general.route('/v1.0/authors', methods=['GET'])
-# def get_author_list():
-#     """
-#     Get author list
-#     ---
-#     responses:
-#             200:
-#                 description: Returns author list
-#                 schema:
-#                   id: AuthorList
-#                   properties:
-#                     code:
-#                       type: string
-#                     message:
-#                       type: string
-#                     authors:
-#                         type: array
-#                         items:
-#                             schema:
-#                                 id: AuthorSummary
-#                                 properties:
-#                                     name:
-#                                         type: string
-#                                     surname:
-#                                         type: string
-#     """
-#     fetched = Author.query.all()
-#     author_schema = AuthorSchema(many=True, only=['name', 'surname'])
-#     authors, error = author_schema.dump(fetched)
-#     return response_with(resp.SUCCESS_200, value={"authors": authors})
-#
-#
-# @route_path_general.route('/v1.0/authors/<int:author_id>', methods=['GET'])
-# def get_author_detail(author_id):
-#     """
-#     Get author detail
-#     ---
-#     parameters:
-#         - name: author_id
-#           in: path
-#           description: ID of the author
-#           required: true
-#           schema:
-#             type: integer
-#
-#     responses:
-#             200:
-#                 description: Returns author detail
-#                 schema:
-#                   id: AuthorList
-#                   properties:
-#                     code:
-#                       type: string
-#                     message:
-#                       type: string
-#                     author:
-#                         id: AuthorFull
-#                         properties:
-#                             name:
-#                                 type: string
-#                             surname:
-#                                 type: string
-#                             books:
-#                                 type: array
-#                                 items:
-#                                     schema:
-#                                         id: BookSchema
-#                                         properties:
-#                                             title:
-#                                                 type: string
-#                                             year:
-#                                                 type: date
-#     """
-#     fetched = Author.query.filter_by(id=author_id).first()
-#     author_schema = AuthorSchema()
-#     author, error = author_schema.dump(fetched)
-#     return response_with(resp.SUCCESS_200, value={"author": author})
-
-
-@route_path_general.route('/v1.0/keys', methods=['GET', 'POST'])
+@route_path_general.route('/v1.0/keys', methods=['POST'])
 def fetch_keys():
     """
     Get github keys
     ---
-
+    consumes:
+    - "application/json"
+    produces:
+    - "application/json"
+    parameters:
+        - name: users
+          in: body
+          description: JSON Blob with a "users" attribute, which contains a list of github usernames. 
+          required: true
+          schema:
+            type: string
     responses:
             200:
                 description: Sucessfully fetched github keys for each username
@@ -188,13 +39,17 @@ def fetch_keys():
                                 id:
                                     type: string
                                 key:
-                                    type: string
+                                    type: string                                
+
     """
+
+    # Read in input from POST
+    data = request.get_json()
+    USERNAMES = data['users']
+
     result = {}
 
     GH_BASE_URL = 'https://api.github.com/'
-    # USERNAMES = ['kennethreitz', 'gvanrossum', 'abaratif']
-    USERNAMES = []
 
     for username in USERNAMES:
 
